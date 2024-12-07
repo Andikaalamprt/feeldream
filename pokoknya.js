053e3eb516d3d3b596d57ee1f385b753061dd4e0
Andikaalamprt <andikaalamprt2005@gmail.com>
2024-12-08 06:40:42 +0700

    monyet:

---
+++ pokoknya.js
@@ -0,0 +1,8 @@
+// Tambahkan di bagian script
+document.addEventListener('DOMContentLoaded', function() {
+    var tombolWA = document.getElementById('tombolWhatsApp');
+    if (tombolWA) {
+        tombolWA.style.display = 'block';
+        tombolWA.style.opacity = '1';
+    }
+});
